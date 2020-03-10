3c584981034301e564d22fc8932d0b6634659443
Georg Schnabel <40870991+gschnabel@users.noreply.github.com>
2020-03-10 17:31:44 +0100

    fixed: full download of EXFOR

--- download_exfor.sh
+++ download_exfor.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 curl --output exfor_urls.txt http://www.nucleardata.com/storage/repos/exfor/exfor_urls.txt
-head -n 2 exfor_urls.txt > exfor_urls2.txt
-xargs -n 1 curl -O < exfor_urls2.txt
+xargs -n 1 curl -O < exfor_urls.txt
 ls *.tar.xz | xargs -n 1 tar -xf
